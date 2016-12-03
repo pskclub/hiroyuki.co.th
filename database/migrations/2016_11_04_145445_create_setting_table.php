2b0d7d5a091aa95c1fdb253e14de223015c6033e
PskCluB <passakon_p@hotmail.com>
2016-12-03 18:04:12 +0700

    add seed setting

--- database/migrations/2016_11_04_145445_create_setting_table.php
+++ database/migrations/2016_11_04_145445_create_setting_table.php
@@ -13,7 +13,10 @@ class CreateSettingTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('setting', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('email');
+        });
     }
 
     /**
---
+++ database/seeds/SettingTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SettingTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('setting')->insert([
+            'email' => 'passakon_p@hotmail.com',
+        ]);
+    }
+}
