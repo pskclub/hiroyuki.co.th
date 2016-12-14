0e2f998c4039d685204aea896cf3cff73692040e
PskCluB <passakon_p@hotmail.com>
2016-12-14 18:07:58 +0700

    add js ediitor to tinymce

--- resources/views/admin/page/setting.blade.php
+++ resources/views/admin/page/setting.blade.php
@@ -189,7 +189,21 @@
 @push('scripts')
 <script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
 <script>tinymce.init({ selector:'textarea' ,  // change this value according to your HTML
-        height : 300});</script>
+        height : 300,
+        theme: 'modern',
+        plugins: [
+            'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+            'searchreplace wordcount visualblocks visualchars code fullscreen',
+            'insertdatetime media nonbreaking save table contextmenu directionality',
+            'paste textcolor colorpicker textpattern imagetools codesample toc'
+        ],
+        toolbar1: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+        toolbar2: 'print preview media | forecolor backcolor emoticons | codesample',
+        image_advtab: true,
+
+        content_css: [
+            '//www.tinymce.com/css/codepen.min.css'
+        ]});</script>
 
 @endpush
 
